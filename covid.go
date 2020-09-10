de500ce32e78ec06b18ea7770336c474e589f882
Manuel <10187614+TheTipo01@users.noreply.github.com>
2020-09-10 15:31:36 +0200

    Using only latest data, refactoring

--- covid.go
+++ covid.go
@@ -8,26 +8,24 @@ import (
 	"time"
 )
 
-const dataUrl = "https://github.com/pcm-dpc/COVID-19/raw/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
-
 type Covid struct {
-	Data                         DateTime `csv:"data"`
-	Stato                        string   `csv:"stato"`
-	Ricoverati_con_sintomi       int      `csv:"ricoverati_con_sintomi"`
-	Terapia_intensiva            int      `csv:"terapia_intensiva"`
-	Totale_ospedalizzati         int      `csv:"totale_ospedalizzati"`
-	Isolamento_domiciliare       int      `csv:"isolamento_domiciliare"`
-	Totale_positivi              int      `csv:"totale_positivi"`
-	Variazione_totale_positivi   int      `csv:"variazione_totale_positivi"`
-	Nuovi_positivi               int      `csv:"nuovi_positivi"`
-	Dimessi_guariti              int      `csv:"dimessi_guariti"`
-	Deceduti                     int      `csv:"deceduti"`
-	Casi_da_sospetto_diagnostico string   `csv:"casi_da_sospetto_diagnostico"`
-	Casi_da_screening            string   `csv:"casi_da_screening"`
-	Totale_casi                  int      `csv:"totale_casi"`
-	Tamponi                      int      `csv:"tamponi"`
-	Casi_testati                 string   `csv:"casi_testati"`
-	Note                         string   `csv:"note"`
+	Data                      DateTime `csv:"data"`
+	Stato                     string   `csv:"stato"`
+	RicoveratiConSintomi      int      `csv:"ricoverati_con_sintomi"`
+	TerapiaIntensiva          int      `csv:"terapia_intensiva"`
+	TotaleOspedalizzati       int      `csv:"totale_ospedalizzati"`
+	IsolamentoDomiciliare     int      `csv:"isolamento_domiciliare"`
+	TotalePositivi            int      `csv:"totale_positivi"`
+	VariazioneTotalePositivi  int      `csv:"variazione_totale_positivi"`
+	NuoviPositivi             int      `csv:"nuovi_positivi"`
+	DimessiGuariti            int      `csv:"dimessi_guariti"`
+	Deceduti                  int      `csv:"deceduti"`
+	CasiDaSospettoDiagnostico string   `csv:"casi_da_sospetto_diagnostico"`
+	CasiDaScreening           string   `csv:"casi_da_screening"`
+	TotaleCasi                int      `csv:"totale_casi"`
+	Tamponi                   int      `csv:"tamponi"`
+	CasiTestati               string   `csv:"casi_testati"`
+	Note                      string   `csv:"note"`
 }
 
 type DateTime struct {
@@ -43,7 +41,7 @@ func (date *DateTime) UnmarshalCSV(csv string) (err error) {
 func getCovid() string {
 	var covid []*Covid
 
-	resp, err := http.Get(dataUrl)
+	resp, err := http.Get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-latest.csv")
 	if err != nil {
 		return ""
 	}
@@ -51,6 +49,5 @@ func getCovid() string {
 	_ = gocsv.Unmarshal(resp.Body, &covid)
 	_ = resp.Body.Close()
 
-	return "Dati del " + monday.Format(covid[len(covid)-1].Data.Time, "2 January 2006", monday.LocaleItIT) + ". Totale positivi: " + strconv.Itoa(covid[len(covid)-1].Totale_positivi) + "; Numero di tamponi effettuati oggi: " + strconv.Itoa(covid[len(covid)-1].Tamponi) + "; Numero di morti oggi: " + strconv.Itoa(covid[len(covid)-1].Deceduti) + "; Incremento di casi rispetto a ieri: " + strconv.Itoa(covid[len(covid)-1].Variazione_totale_positivi)
-
+	return "Dati del " + monday.Format(covid[len(covid)-1].Data.Time, "2 January 2006", monday.LocaleItIT) + ". Totale positivi: " + strconv.Itoa(covid[len(covid)-1].TotalePositivi) + "; Numero di tamponi effettuati oggi: " + strconv.Itoa(covid[len(covid)-1].Tamponi) + "; Numero di morti oggi: " + strconv.Itoa(covid[len(covid)-1].Deceduti) + "; Incremento di casi rispetto a ieri: " + strconv.Itoa(covid[len(covid)-1].VariazioneTotalePositivi)
 }
